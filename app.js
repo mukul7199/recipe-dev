048f58bcf7a65193c96ff08a8dd9ccdc4fcdc308
mukul7199 <mukul7199@gmail.com>
2019-03-13 13:48:22 +0530

    Fixed authenticate middleware

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
   //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
   res.header("Access-Control-Allow-Credentials", true);
   return next();
